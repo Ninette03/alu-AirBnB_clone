aeee9a92ea374072e15231232859c9b941cf2109
Ninette03 <n.agatesi@alustudent.com>
2024-09-07 11:55:05 +0200

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module defines the BaseModel class, which is the base class for all
+models in the AirBnB clone project.
+"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    BaseModel class that defines all common attributes/methods for other classes.
+    """
+
+    def __init__(self):
+        """
+        Initialize a new BaseModel instance.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        """
+        Return a string representation of the BaseModel instance.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Update the updated_at attribute with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Return a dictionary representation of the BaseModel instance.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
