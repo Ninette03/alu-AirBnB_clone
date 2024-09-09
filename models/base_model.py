d95c4499fbc85184c89bf610c2ee6cef7274a0a6
Ninette03 <n.agatesi@alustudent.com>
2024-09-09 10:18:00 +0200

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,14 +11,26 @@ class BaseModel:
     """
     BaseModel class that defines all common attributes/methods for other classes.
     """
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize a new BaseModel instance.
+        
+        Args:
+            *args: Not used.
+            **kwargs: Dictionary of attributes to initialize the instance with.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+    
     
     def __str__(self):
         """
