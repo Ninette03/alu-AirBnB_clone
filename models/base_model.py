31f7fed664c6dcd821e85e07b9d0c0a46c11dae0
Ninette03 <n.agatesi@alustudent.com>
2024-09-09 10:59:44 +0200

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ models in the AirBnB clone project.
 
 import uuid
 from datetime import datetime
+from . import storage
 
 class BaseModel:
     """
@@ -30,6 +31,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
     
     
     def __str__(self):
@@ -43,6 +45,7 @@ class BaseModel:
         Update the updated_at attribute with the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
